05c0673bd141e61579c55aafb167bdda66842b4d
Name <isip_a.v.poznyak@mpt.ru>
2026-07-05 20:35:01 +0300

    first commit

---
+++ BosBelme/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BosBelme.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
