4c9ac3941926ddf90af520866d0dc8e069a4ac05
Name <isip_a.v.poznyak@mpt.ru>
2026-07-18 01:05:10 +0300

    Обновление безопасности

---
+++ BosBelme.Data/Migrations/20260717212417_UpdatePlayerCount.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BosBelme.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePlayerCount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
